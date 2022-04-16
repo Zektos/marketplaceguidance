7135e711276c34408cd6afb5018d56a609474e13
Niclas Rafn Topp Vernersen <niv@projectum.com>
2022-04-16 13:12:28 +0200

    Added comments for the complex stuff

--- src/contexts/dbcontext.js
+++ src/contexts/dbcontext.js
@@ -1,19 +1,51 @@
 const Connection = require('tedious').Connection;
 const Request = require('tedious').Request;
-var TYPES = require('tedious').TYPES;
+// var TYPES = require('tedious').TYPES;
 const config = require('./dbcontextconfig.json');
+
+/*
+This is the database context file, which is the file that contains the database functionality.
+It carries out the tasks that is in context of the database.
+The functions has been written to be asyncronus by making promises that is either resolved with the response of the promise, or rejected by failing to deliver on the promise.
+This is nessecary to make sure valid responses from the T-SQL Queries through the tedious library is resolved for the express library to respond correctly for a given request.
+The functions of this file is primarily referenced in the controllers, which contains the routes (endpoints) for the requests made available by express.
+Though i haven't yet been able to do this synchronously, it might actually be possible, but it is definitly the right way to go with async promises in this case any way.
+This is due to the setup of tedious, and the way it utilises vanilla JS events to deliver the response(s) of the queries executed.
+
+Insted of having a lot of strictly defined functions for specific requests, this has been written to allow for execution of variable requests with optional params.
+This is, for now, split into either queries or so called non-queries.
+Queries, which is called through the "executeQuery" function, is the queries of which you expect a resultset containing some amount of data.
+Ex. a T-SQL select statement giving you any amount of rows of data.
+Non-queries, which is called through the "executeNonQuery" function, is the queries of which you expect a result of either success or failed, and maybe even with an amount of rows affected.
+Ex. a T-SQL insert statement, creating one or multiple rows of data.
+*/
+
 
 const executeQuery = (query, params) => new Promise(
     (resolve, reject) => {
+        // Define response creation variables
         let response = [];
         let rowCounter = 0;
 
         // Create Request
         let req = new Request(query, (err) => {
+            // Reject promise if err, resolve promise with the response if successful
             if (err) reject(err);
             resolve(response);
         });
 
+        // Check if params is included, and if so, loop over them and add them to the request
+        if (params != null) for (let i = 0; i < params.length; i++) {
+            // Define and collect the params provided from the call
+            let paramName = params[i][0];
+            let paramType = params[i][1];
+            let paramValue = params[i][2];
+
+            // Add param to the request
+            req.addParameter(paramName, paramType, paramValue);
+        }
+
+        // JS event to collect the data, if any is responded from the query, and add the data to the response to be resolved when all has been processed
         req.on('row', function(columns) {
             response[rowCounter] = {}
             columns.forEach(function(column) {
@@ -24,11 +56,14 @@ const executeQuery = (query, params) => new Promise(
 
         // Create Connection
         let conn = new Connection(config);
+        // JS event to do something when connection is connected.
         conn.on('connect', (err) => {
+            // Promise is rejected if error occour, otherwise, the previously defined request is executed.
             if (err) reject(err);
             conn.execSql(req)
         });
 
+        // Do the connect to execute the above setup through JS events, and eventually reject or resolve the async promise awaited by the caller (In this example the controller).
         conn.connect();
     });
 
@@ -36,38 +71,34 @@ module.exports.executeQuery = executeQuery;
 
 const executeNonQuery = (query, params) => new Promise(
     (resolve, reject) => {
-        let response = [];
-        let rowCounter = 0;
-
         // Create Request
-        let req = new Request(query, (err) => {
+        let req = new Request(query, (err, rowCounter) => {
+            // Reject promise if err, resolve promise with the response if successful
             if (err) reject(err);
-            resolve(response);
+            resolve(rowCounter);
         });
 
-        for (let i = 0; i < params.length; i++) {
+        // Check if params is included, and if so, loop over them and add them to the request
+        if (params != null) for (let i = 0; i < params.length; i++) {
+            // Define and collect the params provided from the call
             let paramName = params[i][0];
             let paramType = params[i][1];
             let paramValue = params[i][2];
 
+            // Add param to the request
             req.addParameter(paramName, paramType, paramValue);
         }
 
-        req.on('row', function(columns) {
-            response[rowCounter] = {}
-            columns.forEach(function(column) {
-                response[rowCounter][column.metadata.colName] = column.value
-            });
-            rowCounter += 1
-        });
-
         // Create Connection
         let conn = new Connection(config);
+        // JS event to do something when connection is connected.
         conn.on('connect', (err) => {
+            // Promise is rejected if error occour, otherwise, the previously defined request is executed.
             if (err) reject(err);
             conn.execSql(req)
         });
 
+        // Do the connect to execute the above setup through JS events, and eventually reject or resolve the async promise awaited by the caller (In this example the controller).
         conn.connect();
     });
 
--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -3,17 +3,30 @@ const router = express.Router();
 var TYPES = require('tedious').TYPES;
 const dbContext = require('../contexts/dbcontext');
 
-// Get all users
-router.get("/getall", async(req, res) => {
+/*
+This is a controller file, which in this case is to host the routes for the user controller.
+This is the endpoints for requests made in regards to the user table of the database.
 
+Without further explaination, this is mostly just examples of how to utilise different types of T-SQL queries using the database context in dbcontext.js
+Any other vital information is explained in comments on the functions of this file.
+*/
+
+
+// Get all users - Async express route, so that we can await the promise of the async dbcontext function(s)
+router.get("/getall", async(req, res) => {
+    // Create T-SQL Query to be executed for a result to be responded upon
     let getAllTSQL = "SELECT * FROM ProgEksamen.Users"
+    // Execute the query with the executeQuery function, as we expect a resultset of rows of data
     let result = await dbContext.executeQuery(getAllTSQL, null)
 
+    // Respond to the request with the result of the executed query, when the promise has been resolved or rejected.
+    // This should probably be split into responding with something different than 200 if the promise is rejected. I just haven't had the time yet.
     res.status(200).json(result);
 });
 
-// Get first
+// Get first - Async express route, so that we can await the promise of the async dbcontext function(s)
 router.get("/getfirst", async(req, res) => {
+    // Just an example of a different query, but exactly the same as the above
 
     let getAllTSQL = "SELECT TOP(1) * FROM ProgEksamen.Users"
     let result = await dbContext.executeQuery(getAllTSQL, null)
@@ -21,17 +34,46 @@ router.get("/getfirst", async(req, res) => {
     res.status(200).json(result);
 });
 
+// Get user by ID - Async express route, so that we can await the promise of the async dbcontext function(s)
+// This one also uses url paramters for the get request, this is done by using :userId in the route.
+router.get("/:userId", async(req, res) => {
+    // Collect the value of the url param
+    let userid = req.params?.userId ?? null;
+
+    // Respond with an error if the param isn't set but somehow the endpoint is triggered anyway (Should be impossible though).
+    if (iserid === null) res.status(500).send('ERROR IN ID PARAM');
+
+    // Create T-SQL Query to be executed for a result to be responded upon. This time including a variable paramter by adding @xxxxx (in this case @userid) to the query.
+    let getAllTSQL = "SELECT TOP(1) * FROM ProgEksamen.Users WHERE id = @userid"
+    // Execute the query with the ExecuteQuery function, as we expect a resultset of rows of data. This time setting an array corresponding the parameters for the request.
+    // This is done by creating an array of arrays containing the 3 variable: VariableName, VariableType, VariableValue
+    // The params is handled in the function by adding the to the tedious query request.
+    let result = await dbContext.executeQuery(getAllTSQL, [
+        ['userid', TYPES.Int, userid]
+    ]);
+
+    // Respond to the request with the result of the executed query, when the promise has been resolved or rejected.
+    // This should probably be split into responding with something different than 200 if the promise is rejected. I just haven't had the time yet.
+    res.status(200).json(result);
+});
+
 // Create user
 router.post("/create", async(req, res) => {
-    var username = req.body?.username ?? null;
-    var password = req.body?.password ?? null;
-    var email = req.body.email ?? null;
-    var userlevel = req.body?.userlevel ?? null;
-    var followedads = req.body?.followedads ?? null;
+    // Collect the value of the body to use for the query parameters
+    let username = req.body?.username ?? null;
+    let password = req.body?.password ?? null;
+    let email = req.body.email ?? null;
+    let userlevel = req.body?.userlevel ?? null;
+    let followedads = req.body?.followedads ?? null;
 
+    // Validate the body and respond with error if not valid
     if (username === null || password === null || email === null || userlevel === null || followedads === null) res.status(500).send('ERROR IN BODY');
 
+    // Create T-SQL Query to be executed with a non resultset response. This time including multiple variable paramters by adding multiple @xxxxx (In this case @username, @password, @email, @userlevel, @followedads) to the query.
     let createUserTSQL = "INSERT INTO ProgEksamen.Users (ID, Username, Password, Email, Userlevel, Followed_ads) VALUES ((SELECT TOP(1) ID FROM [ProgEksamen].[Users] ORDER BY ID DESC) + 1, @username, @password, @email, @userlevel, @followedads)"
+    // Execute the query with the ExecuteNonQuery function, as we do not expect a resultset of rows of data, but rather a success/failed response and a rowCount. This time setting an array corresponding the parameters for the request.
+    // This is done by creating an array of arrays containing the 3 variable: VariableName, VariableType, VariableValue
+    // The params is handled in the function by adding the to the tedious query request.
     let result = await dbContext.executeNonQuery(createUserTSQL, [
         ['username', TYPES.Text, username],
         ['password', TYPES.Text, password],
@@ -40,6 +82,8 @@ router.post("/create", async(req, res) => {
         ['followedads', TYPES.Int, followedads]
     ])
 
+    // Respond to the request with the result of the executed query, when the promise has been resolved or rejected.
+    // This should probably be split into responding with something different than 200 if the promise is rejected. I just haven't had the time yet.
     res.status(200).json(result);
 });
 
